ab8d4a2c5fdae35dad452cc4725073860ec04ee4
jisu an <ahn0167@naver.com>
2022-01-28 16:55:50 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+// state 만드는법
+  let [글제목,글제목변경] = useState(['React 개발 1일차','React 개발 2일차','React 개발 3일차'])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div> 안쑥이 개발 BLOG</div>
+      </div>
+      <div className="list">
+        <h3> {글제목[0]}</h3>
+        <p> 2022년 1월 27일</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> {글제목[1]}</h3>
+        <p> 2022년 1월 27일</p>
+        <hr/>
+      </div>
+      <div className="list">
+        <h3> {글제목[2]}</h3>
+        <p> 2022년 1월 27일</p>
+        <hr/>
+      </div>
     </div>
   );
 }
