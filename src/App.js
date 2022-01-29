0588b3ad9e2db2f478ae37e3d6fccb9d19700cb6
jisu an <ahn0167@naver.com>
2022-01-29 20:37:16 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import './App.css';
 function App() {
 // state 만드는법
   let [글제목,글제목변경] = useState(['React 개발 1일차','React 개발 2일차','React 개발 3일차'])
+  let [좋아요, 좋아요변경] = useState(0);
+  function 제목바꾸기() { 
+    var newArray = [...글제목]; 
+    if(newArray[0]==='React 개발 1일차'){
+      newArray[0] = 'Vue 개발 1일차';
+    }
+    else{
+      newArray[0] = 'React 개발 1일차';
+    }
+    글제목변경(newArray);
+  }
+  function textlist(){
+    var newArray = [...글제목];
+    newArray.sort();
+    textlist(newArray);
+  }
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <div> 안쑥이 개발 BLOG</div>
       </div>
+      <button onClick={textlist}>글정렬하기</button>
       <div className="list">
-        <h3> {글제목[0]}</h3>
+        <h3> {글제목[0]} <span onClick={ ()=>{좋아요변경(좋아요 + 1)} }>👍🏻</span> {좋아요} </h3><button onClick={제목바꾸기}>글제목변경</button>
         <p> 2022년 1월 27일</p>
         <hr/>
       </div>
